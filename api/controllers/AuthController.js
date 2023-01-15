2c70c499389808619288090007637744e7b92ab6
Anjula Perera <anjulaperera98@gmail.com>
2023-01-15 09:03:37 +0530

    registration done

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -6,7 +6,7 @@ exports.registerUser = (req,res) => {
         if(err){
             return res.status(422).json({
                 success : false,
-                message : "Please enter unique email & username",
+                message : "Registration failed!",
                 data:err
             })
         }else{
@@ -16,5 +16,35 @@ exports.registerUser = (req,res) => {
                 // data:err
             })
         }
+    })
+}
+
+exports.loginUser = (req,res) => {
+    // const user = new User(req.body)
+    User.findOne(
+    {email:req.body.email},
+    (err,user)=>{
+        
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User email not found",
+            })
+        }
+
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+        
+            if(!isMatch){
+                return res.status(400).json({
+                    success:false,
+                    message:"Password is incorrect",
+                })
+            }
+            return res.status(200).json({
+                success:false,
+                message:"Login successful",
+            })
+
+        })
     })
 }
--- api/routes/AuthRoutes.js
+++ api/routes/AuthRoutes.js
@@ -3,4 +3,5 @@ module.exports = function(app){
 
     const AuthController = require('../controllers/AuthController')
     app.post('/register',AuthController.registerUser)
+    app.post('/login',AuthController.loginUser)
 }
